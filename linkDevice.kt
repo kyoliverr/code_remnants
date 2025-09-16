3039730027d1c29de7d1b4af9b172efabc6c41e4
kyO <147101840+kyoliverr@users.noreply.github.com>
2025-09-16 09:10:37 -0300

    Update linkDevice.kt

--- linkDevice.kt
+++ linkDevice.kt
@@ -29,3 +29,34 @@ binding.insideRecyclerView.visibility = View.GONE
 
         binding.insideRecyclerView.visibility = View.VISIBLE
         binding.insideLoader.visibility = View.GONE
+
+
+private fun unlinkDeviceToVehicle(vehicle: Vehicle, deviceId: Int){
+        binding.insideLoader.visibility = View.VISIBLE
+        val permission = "vehicle.unlinkDevice"
+        val unlinkDevicePermission = getPermission(requireContext(), permission)
+        val api = RetrofitInstance.getApiService(requireContext(), permission)
+        if (unlinkDevicePermission) {
+            lifecycleScope.launch {
+                try {
+                    val response = api.unlinkDeviceFromVehicle(deviceId)
+                    if (response.isSuccessful) {
+                        //Log.d("API_SUCCESS", "Status: ${response.body()?.status}, Message: ${response.body()?.text}, Dispositivo $deviceId desvinculado do veículo")
+                        Toast.makeText(requireContext(), "Dispositivo desvinculado", Toast.LENGTH_SHORT).show()
+
+                        val index = vehicle.devices.indexOfFirst { it.id == deviceId }
+                        if (index != -1) {
+                            vehicle.devices.removeAt(index)
+                            (binding.insideRecyclerView.adapter as? VehiclesDeviceAdapter)?.apply { notifyItemRemoved(index) }
+                        }
+                        preLoadAllDevices{tryPopulateLinkSpinner()}
+                    } else {
+                        val errorBody = response.errorBody()?.string()
+                        Log.e("API_ERROR_LINK", "Erro ao desvincular dispositivo $deviceId. Código: ${response.code()}. Erro: $errorBody")
+                    }
+                } catch (e: Exception) {}
+            }
+        }
+        binding.insideLoader.visibility = View.GONE
+        binding.insideRecyclerView.visibility = View.VISIBLE
+    } //Destroy device to vehicle

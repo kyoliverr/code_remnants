95b03d94c4ff2a8ddee1de182bc067e1d5f3151b
kyO <147101840+kyoliverr@users.noreply.github.com>
2025-09-17 19:37:08 -0300

    Update TryPopulateSpinners.kt

--- TryPopulateSpinners.kt
+++ TryPopulateSpinners.kt
@@ -13,3 +13,32 @@ private fun tryPopulateCreateSpinners(vehicle: Vehicle? = null){
             associatedEnterprise.add(SpinnerItem(enterprise.id, enterprise.name)) }
         binding.editVehicleEnterprise.adapter = customSpinnerAdapter(requireContext(), associatedEnterprise.distinctBy { it.id })
     }
+
+
+        val allRoles = allRolesCache
+        val associatedRoles = mutableListOf(SpinnerItem(0, "Cargo*"))
+        allRoles?.forEach { role ->
+            associatedRoles.add(SpinnerItem(role.id, role.name)) }
+        binding.newUserRole.adapter = customSpinnerAdapter(requireContext(), associatedRoles.distinctBy { it.id })
+        binding.newUserRole.setSelection(0)
+
+        val allEnterprises = allEnterprisesCache
+        val associatedEnterprises = mutableListOf(SpinnerItem(0, "Empresa Responsável"))
+        allEnterprises?.forEach { enterprise ->
+            associatedEnterprises.add(SpinnerItem(enterprise.id, enterprise.name)) }
+        binding.newUserEnterprise.adapter = customSpinnerAdapter(requireContext(), associatedEnterprises.distinctBy { it.id })
+        binding.newUserEnterprise.setSelection(0)
+
+        val allRoles = allRolesCache
+        val associatedRoles = mutableListOf(SpinnerItem(0, "Cargo*"))
+        allRoles?.forEach { role ->
+            associatedRoles.add(SpinnerItem(role.id, role.name)) }
+        binding.editUserRole.adapter = customSpinnerAdapter(requireContext(), associatedRoles.distinctBy { it.id })
+
+        val allEnterprises = allEnterprisesCache
+        val associatedEnterprises = mutableListOf(SpinnerItem(0, "Empresa Responsável"))
+        allEnterprises?.forEach { enterprise ->
+            associatedEnterprises.add(SpinnerItem(enterprise.id, enterprise.name)) }
+        binding.editUserEnterprise.adapter = customSpinnerAdapter(requireContext(), associatedEnterprises.distinctBy { it.id })
+    
+

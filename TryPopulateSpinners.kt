ee20ec3eea01e1ea767c94177abaf62566a0de99
kyO <147101840+kyoliverr@users.noreply.github.com>
2025-09-30 20:04:41 -0300

    Update TryPopulateSpinners.kt

--- TryPopulateSpinners.kt
+++ TryPopulateSpinners.kt
@@ -40,5 +40,28 @@ private fun tryPopulateCreateSpinners(vehicle: Vehicle? = null){
         allEnterprises?.forEach { enterprise ->
             associatedEnterprises.add(SpinnerItem(enterprise.id, enterprise.name)) }
         binding.editUserEnterprise.adapter = customSpinnerAdapter(requireContext(), associatedEnterprises.distinctBy { it.id })
+
+        private fun tryPopulateMainUserSpinners(enterprise: Enterprise) {
+        if(binding.hiddenEnterpriseMainUserLayout.visibility != View.VISIBLE) return
+
+        val allUsers = allUsersCache
+        val associatedUsers = mutableListOf<SpinnerItem>()
+        allUsers?.forEach { user ->
+            val sameEnterprise = (user.enterprise.id == enterprise.id)
+            val notMainUser = (user.id != enterprise.mainUser?.id)
+
+            if (sameEnterprise && notMainUser){
+                associatedUsers?.add(SpinnerItem(user.id, user.email ?: ""))
+            }
+        }
+        if (associatedUsers.isEmpty()) {
+            associatedUsers.add(SpinnerItem(0, "Nenhum usuário disponível"))
+        }
+        else { associatedUsers.add(0, SpinnerItem(0, "Selecione um usuário")) }
+
+        Log.d("API_LINK_USERS", "Usuários: ${associatedUsers}") //Remove after
+        binding.linkEnterpriseMainUser.adapter = customSpinnerAdapter(requireContext(), associatedUsers.distinctBy { it.id })
+        binding.linkEnterpriseMainUser.setSelection(0)
+    }
     
 

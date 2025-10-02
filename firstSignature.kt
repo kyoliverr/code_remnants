e58fb0f8b82b538728893949aac9a0848222ce81
kyO <147101840+kyoliverr@users.noreply.github.com>
2025-10-02 16:01:03 -0300

    Create firstSignature.kt

---
+++ firstSignature.kt
@@ -0,0 +1,17 @@
+<TextView
+                        android:layout_width="match_parent"
+                        android:layout_height="wrap_content"
+                        android:textSize="14sp"
+                        android:textStyle="bold"
+                        android:text="Assinatura:"/>
+                    <EditText
+                        android:id="@+id/updateTaskSignature"
+                        android:paddingVertical="10dp"
+                        android:paddingHorizontal="16dp"
+                        android:hint="Vou ver sobre depois"
+                        android:layout_width="match_parent"
+                        android:layout_height="match_parent"
+                        android:background="@drawable/border_edit"
+                        android:elevation="4dp"
+                        android:inputType="number"
+                        android:textColorHint="@color/textColorHint" />
